2b278c1ca5cd24229b7ea460c9553c77c3e33f1b
Jopher <s2100932@usls.edu.ph>
2026-01-27 10:14:52 +0800

    Update home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,5 @@
 @section('content')
     <h1>Welcome to my Portfolio!</h1>
     <p>This is my personal website.</p>
-    <p>Just to let you know John Paul Tuhao is neba</p>
+    <p>Just to let you know John Paul Tuhao is Uhaw</p>
 @endsection
